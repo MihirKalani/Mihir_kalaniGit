9ba81b3e4a9ab18532c4ec39a70d0d34baee9350
Mihir <104824672+MihirKalani@users.noreply.github.com>
2026-03-20 10:35:52 +0530

    Update login credentials in Assignment2.js

--- JavaScript/Assignment2.js
+++ JavaScript/Assignment2.js
@@ -1,20 +1,14 @@
-const username = "dhrumil_parmar";
-const password = "MyN@me$!r0nm@n";
+const username = "mihir_kalani";
+const password = "Mihir@123";
 
-// const inputUsername = prompt("Enter your username:")
-// const inputPassword = prompt("Enter your password:")
-const inputUsername = "dhrumil_parmar";
-const inputPassword = "MyN@me$!r0nm@n";
+const inputUsername = "mihir_kalani";
+const inputPassword = "Mihir@123";
 if (inputUsername === username && inputPassword === password) {
   console.log("Login successful!");
-  // alert("Login successful !");
 } else if (inputUsername !== username || inputPassword === password) {
   console.log(" Something is not correct");
-  // alert("Something is not correct");
 } else if (inputUsername == username || inputPassword !== password) {
   console.log("Something is not correct");
-  // alert("Something is not correct");
 } else {
   console.log("Both incorrect");
-  // alert("Both incorrect");
 }
